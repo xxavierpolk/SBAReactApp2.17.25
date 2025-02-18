f4992f866796128ac21211a41f747849fc232193
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-18 00:02:16 -0500

    Voila Images

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -11,7 +11,7 @@ export default function MovieCard({movie}) {
     return (
         <div className="movie-card">
             <div className="movie-poster">
-                <img src={movie.url} alt={movie.title} />
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-heart">
                     <button className="fav-btn" onClick={onFavoriteClick}>
                     ♥
